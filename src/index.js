0fb6d6b5db9b404b2ceeae00288d460aab38d821
sunnyFan <fx35792@163.com>
2019-07-16 15:41:56 +0800

    react-webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 // import '@babel/polyfill';
-import Header from './header.js';
-import Slider from './slider.js';
-import Content from './content.js';
-import GetImg from './img.js';
-import GetIconFont from './Iconfont.js';
-import ButtonClick from './btn.js';
-import Es6 from './es6.js';
-import './style/index.scss';
-new Header();
-new Slider();
-new Content();
-new GetImg();
-new GetIconFont();
-new ButtonClick();
-new Es6();
-// consele.log(1111);//验证 source-map
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>hello react</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- src/slider.js
+++
@@ -1,7 +0,0 @@
-function Slider(){
-	var dom = document.getElementById('root');
-	var slider = document.createElement('div');
-	slider.innerHTML = 'slider';
-	dom.append(slider);
-}
-export default Slider;
